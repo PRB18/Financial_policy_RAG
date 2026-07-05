86e6fb48a702283760ffc7ccbd287eeafe4fa266
Rishi <baburishi505@gmail.com>
2026-07-05 14:59:51 +0530

    wrote basic agent logic with just placholders

---
+++ agent.py
@@ -0,0 +1,25 @@
+from typing import TypedDict, Annotated, Sequence
+from langchain_core.messages import BaseMessage
+
+#this is the "state"
+#it stores messages list and context
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], "conversation history"]
+
+
+#this is a "node"
+#this node retrieves from the db
+#takes "state" from AgentSate as an input parameter and return the updated state
+def retrirve_from_db(state: AgentState):
+    print("retrieving from the chromaDB....")
+    return {"messages": state["messages"]}
+
+#this node fetches the live data from the internet
+def live_search(state: AgentState):
+    print("Searching the web...")
+    return {"messages": state["messages"]}
+
+#this node comapres the static data from the db with the live data
+def comaprision(state: AgentState):
+    print("Comparing static and live data...")
+    return {"messages": state["messages"]}
